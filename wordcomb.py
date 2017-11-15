60322ab88768e1db446482a49734e6ae656b1a8d
Guestik <lukas.andel@seznam.cz>
2017-11-15 16:06:07 +0100

    Delete wordcomb.py

--- wordcomb.py
+++
@@ -1,8 +0,0 @@
-print "WordComb Script by Guestík";
-listOfWords = list();
-nameOfFile = raw_input("Name of new file: ");
-#If file exists, already existing words in file add to list and then add new words
-#If file doesnt exists, create a new one
-addWord = raw_input("Add word: ");
-listOfWords.append(addWord);
-print "Bye";
